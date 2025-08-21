56074e4d0467057dabd98f70744dad20bab52ca3
Pat-devs <patryk.kodehode@gmail.com>
2025-08-21 11:17:05 +0200

    example array of objects destructuring

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-const person = {
-  name: "Alice",
-  age: 25,
-  city: "Oslo"
-};
+const participants = [
+  {
+    name: "Alice",
+    age: 25,
+    city: "Oslo"
+  },
+  {
+    name: "Bob",
+    age: 30,
+    city: "Bergen"
+  },
+  {
+    name: "Charlie",
+    age: 28,
+    city: "Trondheim"
+  },
+  {
+    name: "Diana",
+    age: 35,
+    city: "Stavanger"
+  }
+];
 
-const { name, age, city, country = "Norway" } = person
+// destructure participats, to show the "top3 winners" (basically the first 3 people in the array)
+const [ winner, secondPlace, thirdPlace ] = participants
 
-console.log(country)
+
+console.log(winner.name)
+console.log("Second place: ",  secondPlace.name)
+console.log("Second place: ",  thirdPlace.name)
+
+// display only the name
